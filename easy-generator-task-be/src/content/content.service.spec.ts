230a41285da7802b0d1214f77f143a3651e0ee43
Ahmed Yasser <ayasser@raisaenergy.com>
2025-10-27 06:56:06 +0300

    [FEAT] centralizing quotes definition to be used in tests and service from same place

--- easy-generator-task-be/src/content/content.service.spec.ts
+++ easy-generator-task-be/src/content/content.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContentService } from './content.service';
+import { QUOTES } from './constants/content.constants';
 
 describe('ContentService', () => {
   let service: ContentService;
@@ -31,27 +32,13 @@ describe('ContentService', () => {
         quotes.add(service.getQuote());
       }
 
-      // Since we have 11 quotes, we should get at least 2 different ones
+      // Since we have 12 quotes, we should get at least 2 different ones
       expect(quotes.size).toBeGreaterThan(1);
     });
 
     it('should return a quote from the predefined list', () => {
-      const validQuotes = [
-        "The only way to do great work is to love what you do. - Steve Jobs",
-        "Innovation distinguishes between a leader and a follower. - Steve Jobs",
-        "Life is what happens to you while you're busy making other plans. - John Lennon",
-        "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-        "It is during our darkest moments that we must focus to see the light. - Aristotle",
-        "The way to get started is to quit talking and begin doing. - Walt Disney",
-        "Spread love everywhere you go. Let no one ever come to you without leaving happier. - Mother Teresa",
-        "Do one thing every day that scares you. - Eleanor Roosevelt",
-        "The only impossible journey is the one you never begin. - Tony Robbins",
-        "Don't judge each day by the harvest you reap but by the seeds that you plant. - Robert Louis Stevenson",
-        "The purpose of our lives is to be happy. - Dalai Lama",
-      ];
-
       const result = service.getQuote();
-      expect(validQuotes).toContain(result);
+      expect(QUOTES).toContain(result);
     });
   });
 });
